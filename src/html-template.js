105570b7187dddd00b8b1952546d6070f579b02f
Andrewy2416 <andrewy2416@gmail.com>
2021-11-24 20:15:55 -0800

    changed html-template to non data-array and added script

--- src/html-template.js
+++ src/html-template.js
@@ -1,34 +1,107 @@
-const generateManagerCards 
+const Manager = require('../lib/Manager');
+const Engineer = require('../lib/Engineer')
+const Intern = require('../lib/Intern')
 
-const generateEngineerCards 
 
-const generateInternCards 
+function generateCards(team) {
+  let cards = []
+  for(let i = 0; i < team.length; i++) {
+    const teamArray = team[i];
+    switch(teamArray.getRole()) {
+      case 'Manager':
+        const manager = new Manager(teamArray.id, teamArray.name, teamArray.email, teamArray.officeNumber);
+        cards.push(generateManagerCard(manager));
+        break;
+      case 'Engineer':
+        const engineer = new Engineer(teamArray.id, teamArray.name, teamArray.email, teamArray.github);
+        cards.push(generateEngineerCard(engineer));
+        break;
+      case 'Intern':
+        const intern = new Intern(teamArray.id, teamArray.name, teamArray.email, teamArray.school);
+        cards.push(generateInternCard(intern));
+        break;
+    }
+  }
+  return cards.join(``)
+}
 
-module.exports = data => {
-    return `
+let generateManagerCard = (manager) => {
+  return `
+  <div class="card m-1 shadow" style="width: 18rem">
+    <div class='card-header'>
+      <h3 class="card-title">${manager.name}</h3>
+    </div>
+    <div class="card-body">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${manager.id}</li>
+        <li class="list-group-item">Email: ${manager.email}</li>
+        <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
+      </ul>
+    </div>
+  </div>
+  `
+}
+
+let generateEngineerCard = (engineer) => {
+  return `
+  <div class="card m-1 shadow" style="width: 18rem">
+    <div class='card-header'>
+      <h3 class="card-title">${engineer.name}</h3>
+    </div>
+    <div class="card-body">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${engineer.id}</li>
+        <li class="list-group-item">Email: ${engineer.email}</li>
+        <li class="list-group-item">GitHub: ${engineer.github}</li>
+      </ul>
+    </div>
+  </div>
+  `
+};
+
+let generateInternCard = (intern) => {
+  return `
+  <div class="card m-1 shadow" style="width: 18rem">
+    <div class='card-header'>
+      <h3 class="card-title">${intern.name}</h3>
+    </div>
+    <div class="card-body">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${intern.id}</li>
+        <li class="list-group-item">Email: ${intern.email}</li>
+        <li class="list-group-item">School: ${intern.school}</li>
+      </ul>
+    </div>
+  </div>
+  `
+}
+
+function generateTeam(team) {
+  console.log(team)
+return `
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link
-        rel="stylesheet"
-        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-        crossorigin="anonymous"
-    />
-    <title>Team Profiles</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  <title>My Team</title>
 </head>
 <body>
-<header>
-    <h1>The Team</h1>
-</header>
-<main>
-    ${generateManagerCards}
-    ${generateEngineerCards}
-    ${generateInternCards}
-</main>
+<div class="jumbotron jumbotron-fluid bg-warning">
+  <div class="container">
+    <h1 class="display-4 text-center">My Team</h1>
+  </div>
+</div>
+<div class="d-flex flex-row flex-wrap justify-content-center">
+${generateCards(team)}
+</div>
 </body>
-</html>   
-`
+</html>
+    `
 }
+
+
+module.exports = generateTeam;
